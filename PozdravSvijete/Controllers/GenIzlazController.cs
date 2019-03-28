3326844409f3f4df188f1fa78b941719b7a8a62a
mikleusevicmarin <mikleusevicmarin@gmail.com>
2019-03-28 18:48:22 +0100

    dodano

---
+++ PozdravSvijete/Controllers/GenIzlazController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PozdravSvijete.Controllers
+{
+    public class GenIzlazController : Controller
+    {
+        // GET: GenIzlaz
+        public ActionResult PopisKosarice()
+        {
+            return View();
+        }
+        public ActionResult ListaArtikala()
+        {
+            string[] lista = new string[] { "Skije", "Pancerice", "Sunčane naočale" };
+            ViewBag.Lista = lista;
+            return View();
+        }
+    }
+}
