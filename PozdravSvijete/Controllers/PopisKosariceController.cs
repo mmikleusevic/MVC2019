32142a98dd525b2878fb5f3a010a4b5f603443a0
programer10 <programer10@ucione.local>
2019-03-29 16:12:02 +0100

    dodan zadatak

---
+++ PozdravSvijete/Controllers/PopisKosariceController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PozdravSvijete.Controllers
+{
+    public class PopisKosariceController : Controller
+    {
+        // GET: PopisKosarice
+        public ActionResult PopisKosarice()
+        {
+            return View();
+        }
+    }
+}
