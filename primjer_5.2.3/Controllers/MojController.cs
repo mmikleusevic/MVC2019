dbe4075634ff4280257d7235f3813419eff4abbe
programer10 <programer10@ucione.local>
2019-04-02 16:26:17 +0200

    dodana razor sintaksa for

---
+++ primjer_5.2.3/Controllers/MojController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace primjer_5._2._3.Controllers
+{
+    public class MojController : Controller
+    {
+        // GET: Moj
+        public ViewResult SwitchView()
+        {
+            return View();
+        }
+        public ViewResult ViewZaForPetlju()
+        {
+            string[] voce =
+            {
+                "Jabuka",
+                "Kruška",
+                "Šljiva",
+                "Gljiva",
+                "Banana"
+            };
+            return View((object)voce);
+        }
+    }
+}
---
+++ primjer_5.2.3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="primjer_5._2._3.MvcApplication" Language="C#" %>
