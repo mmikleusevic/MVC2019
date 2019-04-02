616896c3040ea531711c7724a6a963550df2c5fe
programer10 <programer10@ucione.local>
2019-04-02 17:31:55 +0200

    dodan zadatak

---
+++ 5.3.1_zadatak/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace _5._3._1_zadatak
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ 5.3.1_zadatak/Controllers/LayoutSectionController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _5._3._1_zadatak.Controllers
+{
+    public class LayoutSectionController : Controller
+    {
+        // GET: LayoutSection
+        public ViewResult Stranica1()
+        {
+            return View();
+        }
+        public ViewResult Stranica2()
+        {
+            return View();
+        }
+    }
+}
