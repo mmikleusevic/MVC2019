28bb2159aaf33719bea84ef2e5c19a5580f455b0
programer10 <programer10@ucione.local>
2019-04-03 18:14:26 +0200

    dodani primjeri

---
+++ 4.2.1_narudzba_artikla/Controllers/ChildActionController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _4._2._1_narudzba_artikla.Controllers
+{
+    public class ChildActionController : Controller
+    {
+        // GET: ChildAction
+        public ViewResult ChildActionView()
+        {
+            string[] proizvodi = new string[]
+            {
+                "Banana","Jabuka","Kivi","Mrkva","Kupus"
+            };
+            return View(proizvodi);
+        }
+        [ChildActionOnly]
+        public string OdrediGrupuProizvoda(string proizvod)
+        {
+            switch (proizvod)
+            {
+                case "Jabuka":
+                case "Kivi":
+                case "Banana": return "Voće";
+                default:
+                    return "Povrće";
+            }
+        }
+    }
+}
---
+++ 4.2.1_narudzba_artikla/Models/Osoba.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _4._2._1_narudzba_artikla.Models
+{
+    public class Osoba
+    {
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public string Email { get; set; }
+        public string MjestoStanovanja { get; set; }
+        public string Spol { get; set; }
+        public bool Zaposlen { get; set; }
+    }
+}
