a03860850e4fe5e4add4951030030b889de721cf
programer10 <programer10@ucione.local>
2019-04-04 18:40:29 +0200

    dodan primjer

---
+++ 6.2_primjer/Controllers/MetaDataHtmlHelperiController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using _6._2_primjer.Models;
+
+namespace _6._2_primjer.Controllers
+{
+    public class MetaDataHtmlHelperiController : Controller
+    {
+        // GET: MetaDataHtmlHelperi
+        public ViewResult MetaDataView()
+        {
+            return View(new OsobaMeta());
+        }
+        [HttpPost]
+        public ViewResult MetaDataView(OsobaMeta osoba)
+        {
+            return View("HtmlLabelDisplay", osoba);
+        }
+    }
+}
