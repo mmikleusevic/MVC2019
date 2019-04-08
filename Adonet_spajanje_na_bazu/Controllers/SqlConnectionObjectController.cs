963abb5c4c5f0e5b7bbbc7faf74b44d096c3ae28
mikleusevicmarin <mikleusevicmarin@gmail.com>
2019-04-08 18:03:06 +0200

    dodano

--- Adonet_spajanje_na_bazu/Controllers/SqlConnectionObjectController.cs
+++ Adonet_spajanje_na_bazu/Controllers/SqlConnectionObjectController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
@@ -11,6 +12,7 @@ namespace Adonet_spajanje_na_bazu.Controllers
     public class SqlConnectionObjectController : Controller
     {
         // GET: SqlConnectionObject
+        // Direktno
         public ActionResult Index()
         {
             string connString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=dbAlgebra;Integrated Security=True";
@@ -19,7 +21,35 @@ namespace Adonet_spajanje_na_bazu.Controllers
             try
             {
                 conn.Open();
-                if(conn.State == ConnectionState.Open)
+                if (conn.State == ConnectionState.Open)
+                {
+                    Response.Write("Konekcija uspjela");
+                }
+                else
+                {
+                    Response.Write("Neuspješna konekcija");
+                }
+            }
+            catch (SqlException sqlEx)
+            {
+                Response.Write(sqlEx.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return View();
+        }
+        // Preko Web.Config
+        public ActionResult SpojPrekoWebConfig()
+        {
+            string connString = ConfigurationManager.ConnectionStrings["dbAlgebraConnStrSql"].ConnectionString;
+            SqlConnection conn = new SqlConnection(connString);
+
+            try
+            {
+                conn.Open();
+                if (conn.State == ConnectionState.Open)
                 {
                     Response.Write("Konekcija uspjela");
                 }
