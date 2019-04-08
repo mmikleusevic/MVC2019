49ffc64b371e6a57b3dc9b115690774619bc686a
programer10 <programer10@ucione.local>
2019-04-08 17:45:06 +0200

    spajanje na bazu

---
+++ Adonet_spajanje_na_bazu/Controllers/SqlConnectionObjectController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Adonet_spajanje_na_bazu.Controllers
+{
+    public class SqlConnectionObjectController : Controller
+    {
+        // GET: SqlConnectionObject
+        public ActionResult Index()
+        {
+            string connString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=dbAlgebra;Integrated Security=True";
+            SqlConnection conn = new SqlConnection(connString);
+
+            try
+            {
+                conn.Open();
+                if(conn.State == ConnectionState.Open)
+                {
+                    Response.Write("Konekcija uspjela");
+                }
+                else
+                {
+                    Response.Write("Neuspješna konekcija");
+                }
+            }
+            catch (SqlException sqlEx)
+            {
+                Response.Write(sqlEx.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return View();
+        }
+    }
+}
