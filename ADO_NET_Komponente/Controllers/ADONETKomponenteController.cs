4b25b554ef61be536c4cc766d517aa266106ee87
programer10 <programer10@ucione.local>
2019-04-10 17:50:12 +0200

    zadaca dovrseno

--- ADO_NET_Komponente/Controllers/ADONETKomponenteController.cs
+++ ADO_NET_Komponente/Controllers/ADONETKomponenteController.cs
@@ -36,10 +36,11 @@ namespace ADO_NET_Komponente.Controllers
                         while (dr.Read())
                         {
                             PolaznikModel polaznik = new PolaznikModel();
-                            polaznik.IdPolaznik = int.Parse(dr["IdPolaznik"].ToString());
-                            polaznik.Ime = dr["Ime"].ToString();
-                            polaznik.Prezime = dr["Prezime"].ToString();
-                            polaznik.DatumRodjenja = DateTime.Parse(dr["DatumRodjenja"].ToString());
+                            polaznik.IdPolaznik = int.Parse(dr["id"].ToString());
+                            polaznik.Ime = dr["ime"].ToString();
+                            polaznik.Prezime = dr["prezime"].ToString();
+                            polaznik.Email = dr["email"].ToString();
+                            polaznik.DatumRodjenja = DateTime.Parse(dr["datumRodjenja"].ToString());
                             lstPolaznici.Add(polaznik);
                         }
                     }
@@ -69,12 +70,12 @@ namespace ADO_NET_Komponente.Controllers
         {
             SqlConnection conn = new SqlConnection(connStr);
 
-            string cmdText = "SELECT * from tblPolaznici WHERE IdPolaznik=@IdPolaznik";
+            string cmdText = "SELECT * from tblPolaznici WHERE id=@id";
 
             SqlCommand cmd = new SqlCommand(cmdText,conn);
 
             SqlParameter param = new SqlParameter();
-            param.ParameterName = "@IdPolaznik";
+            param.ParameterName = "@id";
             param.DbType = DbType.Int32;
             param.Direction = ParameterDirection.Input;
             param.Value = IdPolaznik;
@@ -93,10 +94,11 @@ namespace ADO_NET_Komponente.Controllers
                     {
                         while (dr.Read())
                         {
-                            polaznik.IdPolaznik = int.Parse(dr["IdPolaznik"].ToString());
-                            polaznik.Ime = dr["Ime"].ToString();
-                            polaznik.Prezime = dr["Prezime"].ToString();
-                            polaznik.DatumRodjenja = DateTime.Parse(dr["DatumRodjenja"].ToString());
+                            polaznik.IdPolaznik = int.Parse(dr["id"].ToString());
+                            polaznik.Ime = dr["ime"].ToString();
+                            polaznik.Prezime = dr["prezime"].ToString();
+                            polaznik.Email = dr["email"].ToString();
+                            polaznik.DatumRodjenja = DateTime.Parse(dr["datumRodjenja"].ToString());
                         }
                     }
                 }
@@ -133,7 +135,7 @@ namespace ADO_NET_Komponente.Controllers
                 using(SqlConnection conn = new SqlConnection(connStr))
                 {
                     string cmdText = "";
-                    cmdText += "INSERT INTO tblPolaznici (ime,prezime,email,datumRodjenja) VALUES ('"+model.Ime+"','"+model.Prezime+"','"+model.Email+"',"+model.DatumRodjenja.ToString("yyy-MM-dd")+")";
+                    cmdText += "INSERT INTO tblPolaznici (ime,prezime,email,datumRodjenja) VALUES ('"+model.Ime+"','"+model.Prezime+"','"+model.Email+"','"+model.DatumRodjenja.ToString("yyy-MM-dd")+"')";
 
                     SqlCommand cmd = new SqlCommand(cmdText, conn);
                     cmd.Connection.Open();
@@ -146,7 +148,123 @@ namespace ADO_NET_Komponente.Controllers
             {
                 ViewBag.Message = "Greška kod upisa polaznika! Opis: " + ex.Message;
             }
-             return View(model);     
+            return View(model);    
+        }
+        [HttpGet]
+        public ActionResult Edit(int IdPolaznik)
+        {
+            SqlConnection conn = new SqlConnection(connStr);
+
+            string cmdText = "SELECT * from tblPolaznici WHERE id=@id";
+
+            SqlCommand cmd = new SqlCommand(cmdText, conn);
+
+            SqlParameter param = new SqlParameter();
+            param.ParameterName = "@id";
+            param.DbType = DbType.Int32;
+            param.Direction = ParameterDirection.Input;
+            param.Value = IdPolaznik;
+            cmd.Parameters.Add(param);
+
+            SqlDataReader dr = null;
+            PolaznikModel polaznik = new PolaznikModel();
+            try
+            {
+                conn.Open();
+                dr = cmd.ExecuteReader();
+
+                if (dr != null)
+                {
+                    if (dr.HasRows)
+                    {
+                        while (dr.Read())
+                        {
+                            polaznik.IdPolaznik = int.Parse(dr["id"].ToString());
+                            polaznik.Ime = dr["ime"].ToString();
+                            polaznik.Prezime = dr["prezime"].ToString();
+                            polaznik.Email = dr["email"].ToString();
+                            polaznik.DatumRodjenja = DateTime.Parse(dr["datumRodjenja"].ToString());
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = "Greška kod dohvaćanja polaznika! Opis: " + ex.Message;
+            }
+            finally
+            {
+                if (dr != null)
+                {
+                    dr.Close();
+                }
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+                conn.Dispose();
+                cmd.Dispose();
+            }
+            return View(polaznik);
+        }
+        [HttpPost]
+        public ActionResult Edit(PolaznikModel model)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connStr))
+                {
+                    string cmdText = "";
+                    cmdText += "UPDATE tblPolaznici ";
+                    cmdText += "SET ime='"+model.Ime+"',prezime='"+model.Prezime+"',email='"+model.Email+"',datumRodjenja='"+model.DatumRodjenja.ToString("yyy-MM-dd")+"' ";
+                    cmdText += "WHERE id="+model.IdPolaznik;
+
+                    SqlCommand cmd = new SqlCommand(cmdText, conn);
+                    cmd.Connection.Open();
+
+                    int brojDodanihRedaka = cmd.ExecuteNonQuery();
+                    if(brojDodanihRedaka > 0)
+                    {
+                        ViewBag.Message = "Zapis je upisan u bazu!";
+                    }
+                    else
+                    {
+                        ViewBag.Message = "Dogodila se greška!";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = "Greška kod upisa polaznika! Opis: " + ex.Message;
+            }
+            return View(model);
+        }
+        [HttpGet]
+        public ActionResult Delete(int idPolaznik)
+        {
+            SqlConnection conn = new SqlConnection(connStr);
+            string cmdText = "DELETE FROM tblPolaznici Where id="+idPolaznik;
+            SqlCommand cmd = new SqlCommand(cmdText, conn);
+            try
+            {
+                cmd.Connection.Open();
+                int brojBrisanihRedaka = cmd.ExecuteNonQuery();
+                TempData["Message"] = "Broj izbrisanih redaka: " + brojBrisanihRedaka;
+            }
+            catch(Exception ex)
+            {
+                ViewBag.Message = "Greška kod brisanja polaznika! Opis: " + ex.Message;
+            }
+            finally
+            {
+                if(conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+                conn.Dispose();
+                cmd.Dispose();
+            }
+            return RedirectToAction("List");
         }
     }
 }
